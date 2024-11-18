25e76608f7abcbc1aaf690b83bad7a47e9deae53
Lutfulbari <lutfulbari29@gmail.com>
2024-11-18 10:20:42 +0600

    django authentication

--- neyamotagro/settings.py
+++ neyamotagro/settings.py
@@ -98,5 +98,19 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
 
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# SMTP ConFiguration 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'lutfulbarimusa@gmail.com'
+EMAIL_HOST_PASSWORD = 'yljw vkwa hmqj nkzf'
+EMAIL_USE_TLS = 'True'
+
